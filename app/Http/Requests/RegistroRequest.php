267a367d27d02857f34a53d8b9234945fa3dd77a
BladimirT <bladimirtetzaguic@gmail.com>
2024-01-14 19:09:57 -0600

    CORS

---
+++ app/Http/Requests/RegistroRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password as PasswordRules;
+
+class RegistroRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string'],
+            'email' => ['required', 'email', 'unique:user,email'],
+            'password' => [
+                'required',
+                'confirmed',
+                PasswordRules::min(8)->letters()->symbols()->numbers()
+            ]
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name' => 'El nombre es obligatorio',
+            'email.required' => 'El correo es obligatorio',
+            'email.email' => 'El correo no es valido',
+            'email.unique' => 'El usuario ya esta registrado',
+            'password' => 'La contraseña debe contener al menos 8 caracteres, un símbolo y un numero'
+        ];
+    }
+}
