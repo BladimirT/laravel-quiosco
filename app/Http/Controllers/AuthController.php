d57de5ba25337eaa4a6d36877ca2bc6ab869c21b
BladimirT <bladimirtetzaguic@gmail.com>
2024-01-19 21:11:00 -0600

    Pedidos

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegistroRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -27,7 +28,7 @@ class AuthController extends Controller
             'user' => $user
         ];
     }
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
         $data = $request->validated();
 
